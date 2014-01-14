7e6068ddb93a5db2a2b4ad3621dd72e01ab71c7c
bramzandbelt <bramzandbelt@gmail.com>
2014-01-14 11:24:56 -0600

    Added code that identifies hostname, allowing dynamic assignment of rootDir across operating systems

--- script_for_generating_x0_and_con.m
+++ script_for_generating_x0_and_con.m
@@ -1,13 +1,26 @@
-rootDir         = '/Users/bramzandbelt/Documents/PROJECTS/SAM/output/';
+for iSubj = 8:13
 
-iSubj           = 8;
-choiceMechType  = 'race';
-inhibMechType   = 'race';
-condParam       = 'v';
+choiceMechType  = 'li';
+inhibMechType   = 'li';
+condParam       = 'zc';
 simGoal         = 'optimize';
 simScope        = 'go';
 solverType      = 'fminsearchcon';
 nStartPoints    = 19;
+
+[~,hostName] = system('hostname');
+switch lower(strtrim(hostName))
+  case 'bram-zandbelts-macbook-pro-2.local'
+    rootDir     = '/Users/bramzandbelt/Documents/Dropbox/SAM/data/';
+  case 'dhcp-129-59-230-168.n1.vanderbilt.edu'
+    rootDir     = '/Users/bramzandbelt/Dropbox/SAM/data/';
+  otherwise
+    if regexp(hostName,'.*vampire')
+      rootDir   = '/scratch/zandbeb/sam/';
+    else
+      error('Unknown host')
+    end
+end
 
 % Get bounds and X0
 [LB,UB,X0,tg,linConA,linConB,nonLinCon] = sam_get_bnds(choiceMechType,inhibMechType,condParam,simGoal,simScope,solverType,iSubj);
@@ -27,3 +40,5 @@ save(fullfile(rootDir,sprintf('subj%.2d',iSubj),fNameX0),'X0','tg');
 fNameCon = sprintf('constraints_%strials_c%s_i%s_p%s.mat',simScope, ...
                 choiceMechType,inhibMechType,condParam);
 save(fullfile(rootDir,sprintf('subj%.2d',iSubj),fNameCon),'LB','UB','linConA','linConB','nonLinCon');              
+
+end
