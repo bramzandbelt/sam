5fa7b7d8c798e51650b56044a4e7d0e931ed6d78
bramzandbelt <bramzandbelt@gmail.com>
2014-03-11 13:18:01 -0500

    Added possibility to generate MEX function from sam_spec_timing_diagram

--- sam_mex_codegen.m
+++ sam_mex_codegen.m
@@ -24,58 +24,74 @@ cfg.MATLABSourceComments = true;
 switch lower(task)
   case 'sam_sim_trial'
     codegen_sam_sim_trial;
+  case 'sam_spec_timing_diagram'
+    codegen_sam_spec_timing_diagram
 end
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 3. NESTED FUNCTIONS
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
+function codegen_sam_spec_timing_diagram
+
+  % This function compiles the sam_spec_timing_diagram.m file to a MEX file
+  
+  ons           = coder.typeof(0,[5e1,5e1],[1,1]);
+  dur           = coder.typeof(0,[5e1,5e1],[1,1]);
+  v             = coder.typeof(0,[5e1,1],[1,0]);
+  se            = coder.typeof(0,[5e1,1],[1,0]);
+  dt            = coder.typeof(0,[1,1],[0,0]);
+  tWindow       = coder.typeof(0,[1,2],[0,0]);
+  
+  codegen sam_spec_timing_diagram -config cfg -args {ons,dur,v,se,dt,tWindow}
+  
+end
 
 function codegen_sam_sim_trial
 
-% This function compiles the epu_n_unit_lba.m file to a MEX file
-u           = coder.typeof(0,[5e1,1e6],[1,1]);
-A           = coder.typeof(0,[5e1,5e1],[1,1]);
-B           = coder.typeof(0,[5e1,5e1,1e3],[1,1,1]);
-C           = coder.typeof(0,[5e1,1e3],[1,1]);
-D           = coder.typeof(0,[5e1,5e1,5e1],[1,1,1]);
-Sin         = coder.typeof(0,[5e1,5e1],[1,1]);
-Z0          = coder.typeof(0,[5e1,1],[1,0]);
-ZC          = coder.typeof(0,[5e1,1],[1,0]);
-ZLB         = coder.typeof(0,[5e1,1],[1,0]);
-dt          = coder.typeof(0,[1,1],[0,0]);
-tau         = coder.typeof(0,[1,1],[0,0]);
-T           = coder.typeof(0,[1,1e6],[0,1]);
-terminate   = coder.typeof(true,[5e1,1],[1,0]);
-blockInput  = coder.typeof(true,[5e1,5e1],[1,1]);
-latInhib    = coder.typeof(true,[5e1,5e1],[1,1]);
-n           = coder.typeof(0,[1,1],[0,0]);
-m           = coder.typeof(0,[1,1],[0,0]);
-p           = coder.typeof(0,[1,1],[0,0]);
-t           = coder.typeof(0,[1,1],[0,0]);
-rt          = coder.typeof(0,[5e1,1],[1,0]);
-resp        = coder.typeof(true,[5e1,1],[1,0]);
-z           = coder.typeof(0,[5e1,1e6],[1,1]);
+  % This function compiles the sam_sim_trial.m file to a MEX file
+  u           = coder.typeof(0,[5e1,1e6],[1,1]);
+  A           = coder.typeof(0,[5e1,5e1],[1,1]);
+  B           = coder.typeof(0,[5e1,5e1,1e3],[1,1,1]);
+  C           = coder.typeof(0,[5e1,1e3],[1,1]);
+  D           = coder.typeof(0,[5e1,5e1,5e1],[1,1,1]);
+  Sin         = coder.typeof(0,[5e1,5e1],[1,1]);
+  Z0          = coder.typeof(0,[5e1,1],[1,0]);
+  ZC          = coder.typeof(0,[5e1,1],[1,0]);
+  ZLB         = coder.typeof(0,[5e1,1],[1,0]);
+  dt          = coder.typeof(0,[1,1],[0,0]);
+  tau         = coder.typeof(0,[1,1],[0,0]);
+  T           = coder.typeof(0,[1,1e6],[0,1]);
+  terminate   = coder.typeof(true,[5e1,1],[1,0]);
+  blockInput  = coder.typeof(true,[5e1,5e1],[1,1]);
+  latInhib    = coder.typeof(true,[5e1,5e1],[1,1]);
+  n           = coder.typeof(0,[1,1],[0,0]);
+  m           = coder.typeof(0,[1,1],[0,0]);
+  p           = coder.typeof(0,[1,1],[0,0]);
+  t           = coder.typeof(0,[1,1],[0,0]);
+  rt          = coder.typeof(0,[5e1,1],[1,0]);
+  resp        = coder.typeof(true,[5e1,1],[1,0]);
+  z           = coder.typeof(0,[5e1,1e6],[1,1]);
 
-codegen sam_sim_trial_crace_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_crace_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_crace_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_irace_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_ibi_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_ili_nomodbd -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
 
-codegen sam_sim_trial_crace_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_crace_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_crace_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cffi_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
-codegen sam_sim_trial_cli_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_crace_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cffi_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_irace_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_ibi_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
+  codegen sam_sim_trial_cli_ili_nomodbd_inpdepnoise -config cfg -args {u,A,B,C,D,Sin,Z0,ZC,ZLB,dt,tau,T,terminate,blockInput,latInhib,n,m,p,t,rt,resp,z}
 
 end
 
