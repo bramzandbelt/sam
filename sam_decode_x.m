99dbcd16c3411200d32303b447ea05ae3aa160e8
bramzandbelt <bramzandbelt@gmail.com>
2014-03-11 13:17:23 -0500

    Dynamic attribution of model parameter values and changes in variable naming
    The attribution of model parameter values extracted from X0 is now
    dynamic. Model architecture (i.e. choice mechanism and inhibition
    mechanism) is now derived directly from the connectivity matrices,
    where needed, rather than from a text field in the SAM structure.

--- sam_decode_x.m
+++ sam_decode_x.m
@@ -1,5 +1,7 @@
-function [A,B,C,D,V,SE,SI,Z0,ZC,accumOns] = ...
-          sam_decode_x(SAM,X,stimOns,stimDur,N,M,VCor,VIncor,S)
+function [endoConn,extrMod,exoConn,intrMod,V,SE,SI,Z0,ZC,T0] = ...
+          sam_decode_x(SAM,X,iTrial)
+% function [endoConn,extrMod,exoConn,intrMod,V,SE,SI,Z0,ZC,accumOns] = ...
+%           sam_decode_x(SAM,X,iTrial)
 % SAM_DECODE_X <Synopsis of what this function does> 
 %  
 % DESCRIPTION 
@@ -28,31 +30,39 @@ function [A,B,C,D,V,SE,SI,Z0,ZC,accumOns] = ...
 % 1.1. Process inputs
 % ========================================================================= 
 
-% Choice mechanism type
-choiceMechType = SAM.des.choiceMech.type;
+% Column indices
+iZ0       = SAM.model.XCat.i.iZ0;
+iZc       = SAM.model.XCat.i.iZc;
+iV        = SAM.model.XCat.i.iV;
+iVe       = SAM.model.XCat.i.iVe;
+iT0       = SAM.model.XCat.i.iT0;
+iSe       = SAM.model.XCat.i.iSe;
+iSi       = SAM.model.XCat.i.iSi;
+iK        = SAM.model.XCat.i.iK;
+iW        = SAM.model.XCat.i.iW;
 
-% Inhibition mechanism type
-inhibMechType = SAM.des.inhibMech.type;
+simScope  = SAM.sim.scope;
 
-% Parameter that varies across task conditions
-condParam = SAM.des.condParam;
+switch lower(simScope)
+  case 'go'
+    N         = SAM.expt.nRsp(1);
+    M         = SAM.expt.nStm(1);
+  case 'all'
+    N         = SAM.expt.nRsp;
+    M         = SAM.expt.nStm;
+end
 
-% Scope of the simulation
-simScope = SAM.sim.scope;
+trialCat    = SAM.optim.obs.trialCat{iTrial};
+stmOns      = SAM.optim.obs.onset{iTrial};
+stmDur      = SAM.optim.obs.duration{iTrial};
 
-% Number of conditions
-nCnd  = SAM.des.expt.nCnd;
+iTarget     = SAM.optim.modelMat.iTarget{iTrial};
+iNonTarget  = SAM.optim.modelMat.iNonTarget{iTrial};
+exoConn     = SAM.optim.modelMat.exoConn{iTrial};
 
-% Indices of GO inputs, per condition
-iGO   = SAM.des.iGO;
-
-
-
-iZ0 = SAM.des.XCat.i.iZ0;
-iZc = SAM.des.XCat.i.iZc;
-iV  = SAM.des.XCat.i.iV;
-iK  = SAM.des.XCat.i.iK;
-
+% #.#.#. Model matrices
+% -------------------------------------------------------------------------------------------------------------------------
+endoConn  = SAM.model.mat.endoConn;
 
 % 1.2. Specify dynamic variables
 % ========================================================================= 
@@ -60,45 +70,102 @@ iK  = SAM.des.XCat.i.iK;
 trueN = arrayfun(@(x) true(x,1),N,'Uni',0);
 trueM = arrayfun(@(x) true(x,1),M,'Uni',0);
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-% 2. CONVERT X TO INDIVIDUAL PARAMETERS
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
-
-
-switch simScope
-  case 'go'
-    switch choiceMechType
-      case 'li'
-        
-        z0  = X(model.XSpec.i.iCat{iZ0});
-        zc  = X(model.XSpec.i.iCat{iZc});
-        v   = X(model.XSpec.i.iCat{iV});
-        ve  = X(model.XSpec.i.iCat{iVe});
-        t0  = X(model.XSpec.i.iCat{iT0});
-        si  = X(model.XSpec.i.iCat{iSi});
-        se  = X(model.XSpec.i.iCat{iSe});
-        k   = X(model.XSpec.i.iCat{iK});
-        w   = X(model.XSpec.i.iCat{iW});
-        
-        
-        
-      otherwise
-    end
-  case 'all'
-    switch choiceMechType
-      case 'li'
-      otherwise
-        switch inhibMechType
-          case 'li'
-          otherwise
-        end
-    end
+% Parse trial type 
+if ~isempty(regexp(trialCat,'^goTrial_', 'once'))
+  token = regexp(trialCat,'goTrial_(\S*)','tokens');
+  tagGO   = token{1}{1};
+elseif ~isempty(regexp(trialCat,'^stopTrial_', 'once'))
+  token = regexp(trialCat,'stopTrial_ssd(\w*)_(\S*)_(\S*)','tokens');
+%   tagSsd  = str2double(token{1}{1});
+  tagGO   = token{1}{2};
+  tagSTOP = token{1}{3};
 end
+
+% 1.2.1. Parameter indices per task factor
+% -------------------------------------------------------------------------------------------------------------------------
+% Specifies which specific index we need per task factor and accumulator category. A nan means no specific index, a number means a specific index.
+
+indexMat = nan(3,2); 
+
+if ~isempty(regexp(tagGO,'{GO}', 'once'))
+elseif ~isempty(regexp(tagGO,'{GO:s+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:s(\d+)}','tokens');
+  indexMat(1,1) = str2double(indices{1}{1});
+elseif ~isempty(regexp(tagGO,'{GO:r+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:r(\d+)}','tokens');
+  indexMat(2,1) = str2double(indices{1}{1});
+elseif ~isempty(regexp(tagGO,'{GO:c+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:c(\d+)}','tokens');
+  indexMat(3,1) = str2double(indices{1}{1});
+elseif ~isempty(regexp(tagGO,'{GO:s+\d,r+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:s(\d+),r(\d+)','tokens');
+  indexMat(1,1) = str2double(indices{1}{1});
+  indexMat(2,1) = str2double(indices{1}{2});
+elseif ~isempty(regexp(tagGO,'{GO:s+\d,c+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:s(\d+),c(\d+)}','tokens');
+  indexMat(1,1) = str2double(indices{1}{1});
+  indexMat(3,1) = str2double(indices{1}{2});
+elseif ~isempty(regexp(tagGO,'{GO:r+\d,c+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:r(\d+),c(\d+)}','tokens');
+  indexMat(2,1) = str2double(indices{1}{1});
+  indexMat(3,1) = str2double(indices{1}{2});
+elseif ~isempty(regexp(tagGO,'{GO:s+\d,r+\d,c+\d}', 'once'))
+  indices = regexp(tagGO,'{GO:s(\d+),r(\d+),c(\d+)}','tokens');
+  indexMat(1,1) = str2double(indices{1}{1});
+  indexMat(2,1) = str2double(indices{1}{2});
+  indexMat(3,1) = str2double(indices{1}{3});
+end
+
+if ~isempty(regexp(trialCat,'^stopTrial_', 'once'))
+  if ~isempty(regexp(tagSTOP,'{STOP}', 'once'))
+  elseif ~isempty(regexp(tagSTOP,'{STOP:s+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:s(\d+)}','tokens');
+    indexMat(1,2) = str2double(indices{1}{1});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:r+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:r(\d+)}','tokens');
+    indexMat(2,2) = str2double(indices{1}{1});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:c+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:c(\d+)}','tokens');
+    indexMat(3,2) = str2double(indices{1}{1});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:s+\d,r+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:s(\d+),r(\d+)','tokens');
+    indexMat(1,2) = str2double(indices{1}{1});
+    indexMat(2,2) = str2double(indices{1}{2});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:s+\d,c+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:s(\d+),c(\d+)}','tokens');
+    indexMat(1,2) = str2double(indices{1}{1});
+    indexMat(3,2) = str2double(indices{1}{2});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:r+\d,c+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:r(\d+),c(\d+)}','tokens');
+    indexMat(2,2) = str2double(indices{1}{1});
+    indexMat(3,2) = str2double(indices{1}{2});
+  elseif ~isempty(regexp(tagSTOP,'{STOP:s+\d,r+\d,c+\d}', 'once'))
+    indices = regexp(tagSTOP,'{STOP:s(\d+),r(\d+),c(\d+)}','tokens');
+    indexMat(1,2) = str2double(indices{1}{1});
+    indexMat(2,2) = str2double(indices{1}{2});
+    indexMat(3,2) = str2double(indices{1}{3});
+  end
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. CONVERT X TO INDIVIDUAL PARAMETERS
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+z0 = get_value_per_xcat(SAM,X,indexMat,iZ0);
+zc = get_value_per_xcat(SAM,X,indexMat,iZc);
+v  = get_value_per_xcat(SAM,X,indexMat,iV);
+ve = get_value_per_xcat(SAM,X,indexMat,iVe);
+t0 = get_value_per_xcat(SAM,X,indexMat,iT0);
+se = get_value_per_xcat(SAM,X,indexMat,iSe);
+si = get_value_per_xcat(SAM,X,indexMat,iSi);
+k  = get_value_per_xcat(SAM,X,indexMat,iK);
+w  = get_value_per_xcat(SAM,X,indexMat,iW);
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 3. ENCODE CONNECTIVITY MATRICES
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+% Consider adding an additional parameter class to distinguish lateral inhibition within and between accumulator classes
 
 % Endogenous connectivity
 endoConnSelf          = endoConn.self * diag(blkdiag(trueN{:}) * k(:));
@@ -106,246 +173,100 @@ endoConnNonSelfSame   = endoConn.nonSelfSame * diag(blkdiag(trueN{:}) * w(:));
 endoConnNonSelfOther  = endoConn.nonSelfOther * diag(blkdiag(trueN{:}) * w(:));
 endoConn              = endoConnSelf + endoConnNonSelfSame + endoConnNonSelfOther;
 
-% Exogneous connectivity
-exoConn               = exoConn.mat;
-
-% Think of how to incorporate different set sizes, without having to vary
-% exoConn across different cells
-
-
-
-% switch choiceMechType
-%   case 'race'
-%     
-%     % Connectivity to self (leakage)
-%     boolAS = logical(eye(sum(N)));
-%     AS = boolAS*diag(blkdiag(trueN{:})*k(:));
-%     
-%     AOs = zeros(sum(N),sum(N));
-%     
-%     % No feed-forward inhibition
-%     wFFI = repmat({0},nCnd,1);
-%     
-%   case 'ffi'
-%     
-%     % Connectivity to self (leakage)
-%     boolAS = logical(eye(sum(N)));
-%     AS = boolAS*diag(blkdiag(trueN{:})*k(:));
-%     
-%     AOs = zeros(sum(N),sum(N));
-%         
-%     % Feed-forward inhibition: normalized ffi of go-signals to all non-target GO units
-%     wFFI = cellfun(@(a) -1./(numel(a)-1),SAM.des.iGO(:),'Uni',0);
-%         
-%   case 'li'
-%     
-%     % Connectivity to self (leakage)
-%     boolAS = logical(eye(sum(N)));
-%     AS = boolAS*diag(blkdiag(trueN{:})*k(:));
-%     
-%     % Connectivity to other units of same class
-%     boolAOs = blkdiag(trueN{:})*blkdiag(trueN{:})' - boolAS;
-%     AOs = boolAOs*diag(blkdiag(trueN{:})*w(:));
-%     
-%     % No feed-forward inhibition
-%     wFFI = repmat({0},nCnd,1);
-%     
-% end
-% 
-% switch lower(simScope)
-%   case 'all'
-%     switch lower(inhibMechType)
-%       case 'li'
-% 
-%         % Lateral inhibition to other units of other class
-%         boolAOo = ~blkdiag(trueN{:})*blkdiag(trueN{:})';
-%         AOo = boolAOo*diag(blkdiag(trueN{:})*w(:));
-% 
-%       otherwise
-% 
-%         % No lateral inhibition between GO and STOP
-%         AOo = zeros(sum(N),sum(N));
-% 
-%     end
-%   otherwise
-%     
-%     % No lateral inhibition between GO and STOP
-%     AOo = zeros(sum(N),sum(N));
-% end
-% 
-% % Ednogenous connectivity matrix
-% A = AS + AOs + AOo;
-% 
-% % Exogneous connectivity matrix
-% % =========================================================================
-% % The number of units differs across conditions. When the choice mechanism
-% % is feed-forward inhibition, so does the feed-forward inhibition weight.
-% 
-% C = cell(nCnd,1);
-% for iCnd = 1:nCnd
-%   trueIGO = zeros(1,N(1));
-%   trueIGO(iGO{iCnd}) = true;
-%   CGo = wFFI{iCnd}*(trueIGO(:)*trueIGO(:)'-diag(trueIGO)) + diag(trueIGO);
-%   
-%   switch lower(simScope)
-%   case 'go'
-%     C{iCnd} = blkdiag(CGo);
-%   case 'all'
-%     CStop = 1;
-%     C{iCnd} = blkdiag(CGo,CStop);
-%   end
-% end
-
-% Extrinsic and intrinsic modulation
-B = zeros(sum(N),sum(N),sum(M));
-D = zeros(sum(N),sum(N),sum(N));
+% Extrinsic and intrinsic modulation 
+extrMod = zeros(sum(N),sum(N),sum(M));
+intrMod = zeros(sum(N),sum(N),sum(N));
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 4. ENCODE STARTING POINT MATRIX
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-Z0 = blkdiag(trueN{:})*z0(:);
+Z0 = blkdiag(trueN{:}) * z0(:);
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 5. ENCODE THRESHOLD MATRIX
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-ZC = cell(nCnd,1);
-switch lower(condParam)
-  case 'zc'
-    
-    switch lower(simScope)
-      % #.#.#. Optimize go trials only
-      % -------------------------------------------------------------------
-      case 'go'
-        zcc1 = zc(1);
-        ZC{1,1} = blkdiag(trueN{:})*zcc1(:);
-        zcc2 = zc(2);
-        ZC{2,1} = blkdiag(trueN{:})*zcc2(:);
-        zcc3 = zc(3);
-        ZC{3,1} = blkdiag(trueN{:})*zcc3(:);
-        
-      % #.#.#. Optimize all trials
-      % -------------------------------------------------------------------
-      case 'all'
-        zcc1 = zc([1,4]);
-        ZC{1,1} = blkdiag(trueN{:})*zcc1(:);
-        zcc2 = zc([2,4]);
-        ZC{2,1} = blkdiag(trueN{:})*zcc2(:);
-        zcc3 = zc([3,4]);
-        ZC{3,1} = blkdiag(trueN{:})*zcc3(:);
-    end
-    
-  otherwise
-    ZC = cellfun(@(a) blkdiag(trueN{:})*zc(:),ZC,'Uni',0);
-end
+ZC = blkdiag(trueN{:}) * zc(:);
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 6. ACCUMULATION RATES
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-switch condParam
-  
-  % #.#. Rate varies between conditions
-  % =======================================================================
-  case 'v' 
-    
-    switch lower(simScope)
-      
-      
-      % #.#.#. Optimize go trials only
-      % -------------------------------------------------------------------
-      case 'go'
-        vc1 = vcor(1); % rate target go-signal in condition 1
-        vc2 = vcor(2); % rate target go-signal in condition 2
-        vc3 = vcor(3); % rate target go-signal in condition 3
-      
-        vi1 = vincor(1); % rate non-target go-signal in condition 1
-        vi2 = vincor(2); % rate non-target go-signal in condition 2
-        vi3 = vincor(3); % rate non-target go-signal in condition 3
-        
-        
-      % #.#.#. Optimize all trials
-      % -------------------------------------------------------------------
-      case 'all'
-        vc1 = vcor([1,4]); % rate go-signal and stop-signal in condition 1
-        vc2 = vcor([2,4]); % rate go-signal and stop-signal in condition 2
-        vc3 = vcor([3,4]); % rate go-signal and stop-signal in condition 3
-        
-        vi1 = [vincor(1),0]; % rate non-target go-signal and stop-signal in condition 1
-        vi2 = [vincor(2),0]; % rate non-target go-signal and stop-signal in condition 2
-        vi3 = [vincor(3),0]; % rate non-target go-signal and stop-signal in condition 3
-        
-    end
-    
-    % Rates in condition 1
-    V(1,:) = cellfun(@(a,b) a*vc1(:) + b*vi1(:),VCor(1,:),VIncor(1,:),'Uni',0);
-    
-    % Rates in condition 2
-    V(2,:) = cellfun(@(a,b) a*vc2(:) + b*vi2(:),VCor(2,:),VIncor(2,:),'Uni',0);
-    
-    % Rates in condition 3
-    V(3,:) = cellfun(@(a,b) a*vc3(:) + b*vi3(:),VCor(3,:),VIncor(3,:),'Uni',0);
-  
-  % #.#. Rate does not vary between conditions
-  % =======================================================================
-  otherwise
-    
-    switch lower(simScope)
-      case 'go'
-      case 'all'
-        vincor = [vincor,0];
-    end
-    
-    V = cellfun(@(a,b) a*vcor(:) + b*vincor(:),VCor,VIncor,'Uni',0);
-    
-end
+V = blkdiag(iTarget{:}) * v + blkdiag(iNonTarget{:}) * ve;
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 7. EXTRINSIC AND INTRINSIC NOISE
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-switch lower(simScope)
-  case 'go'
-    SE = cellfun(@(a) a*se,S,'Uni',0);
-    SI = cellfun(@(a) a*si,S,'Uni',0); 
-  case 'all'
-    SE = cellfun(@(a) a*[se se]',S,'Uni',0);
-    SI = cellfun(@(a) a*[si si]',S,'Uni',0); 
-end
+SE = diag(blkdiag(trueM{:}) * se(:));
+SI = diag(blkdiag(trueN{:}) * si(:));
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 8. SPECIFY ONSETS AND DURATIONS
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-% Accumulation
-% =========================================================================
-switch condParam
-  
-  case 't0' % Non-decision time varies between conditions
+T0 = blkdiag(trueM{:}) * t0(:);
+
+% THIS NEEDS TO BE IMPLEMENTED
+
+% thisAccumDur = stimDur{iCnd,iTrType}(:)';
+%     
+%     % 4.2.1. Adjust duration of the STOP process, if needed
+%     % ---------------------------------------------------------------------
+%     if iTrType > 1
+%       switch durationSTOP
+%         case 'trial'
+%           % STOP accumulation process lasts the entire trial
+%           thisAccumDur(iSTOP) = timeWindow(2) - thisAccumOns(iSTOP);
+%       end
+%     end
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% #. NESTED FUNCTIONS
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+function xval = get_value_per_xcat(SAM,X,iMat,iCol)
     
-    switch lower(simScope)
+    switch lower(SAM.sim.scope)
       case 'go'
-        t0c1 = t0(1);
-        t0c2 = t0(2);
-        t0c3 = t0(3);
+        xval = SAM.model.XCat.valExcluded(iCol)*ones(1,1);
       case 'all'
-        t0c1 = t0([1,4]);
-        t0c2 = t0([2,4]);
-        t0c3 = t0([3,4]);
+        xval = SAM.model.XCat.valExcluded(iCol)*ones(2,1);
     end
     
-    % Condition 1
-    accumOns(1,:) = cellfun(@(a) a + blkdiag(trueM{:})*t0c1(:),stimOns(1,:),'Uni',0);
+    % Vector of parameter category values in X
+    valuesGO = X(SAM.model.variants.toFit.XSpec.i.iCatClass{1,iCol});
     
-    % Condition 2
-    accumOns(2,:) = cellfun(@(a) a + blkdiag(trueM{:})*t0c2(:),stimOns(2,:),'Uni',0);
+    % Signature
+    signatureGO = SAM.model.variants.toFit.features(:,iCol,1);
     
-    % Condition 3
-    accumOns(3,:) = cellfun(@(a) a + blkdiag(trueM{:})*t0c3(:),stimOns(3,:),'Uni',0);
+    if ~isempty(valuesGO) % Excluded value is used when valuesGO is empty
+      if sum(signatureGO) > 1  
+      elseif sum(signatureGO) == 1
+        xval(1) = valuesGO(iMat(signatureGO,1));
+      elseif sum(signatureGO) == 0
+        xval(1) = valuesGO;
+      end
+    end
+    
+    switch lower(SAM.sim.scope)
+      case 'all'
         
-  otherwise % Non-decision time does not vary between conditions
-    
-    accumOns = cellfun(@(a) a + blkdiag(trueM{:})*t0(:),stimOns,'Uni',0);
-    
-end
+        % Vector of parameter category values in X
+        valuesSTOP = X(SAM.model.variants.toFit.XSpec.i.iCatClass{2,iCol});
+
+        % Signature
+        signatureSTOP = SAM.model.variants.toFit.features(:,iCol,2);
+        
+        if ~isempty(valuesSTOP) % Excluded value is used when valuesSTOP is empty
+          if sum(signatureSTOP) > 1  
+          elseif sum(signatureSTOP) == 1
+            xval(2) = valuesSTOP(iMat(signatureSTOP,1));
+          elseif sum(signatureSTOP) == 0
+            xval(2) = valuesSTOP;
+          end
+        end
+    end    
+  
