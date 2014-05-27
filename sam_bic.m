ed81ece9eb8a7077fbd90eb05991459c0246e6de
bramzandbelt <bramzandbelt@gmail.com>
2014-05-27 14:02:19 -0500

    Renamed variables

--- sam_bic.m
+++ sam_bic.m
@@ -1,15 +1,15 @@
-function bic = sam_bic(pMObs,pMPrd,fObs,nFree)
+function bic = sam_bic(pMObs,pMPrd,nObs,nFree)
 % SAM_BIC <Synopsis of what this function does> 
 %  
 % DESCRIPTION 
 % <Describe more extensively what this function does> 
 %  
 % SYNTAX 
-% bic = SAM_BIC(pMObs,pMPrd,fObs,nFree); 
+% bic = SAM_BIC(pMObs,pMPrd,nObs,nFree); 
 %  
 % pMObs       observed probability mass
 % pMPrd       predicted probability mass
-% fObs        observed frequency
+% nObs        observed frequency
 % nFree       number of free parameters
 %
 % REFERENCES 
@@ -20,5 +20,5 @@ function bic = sam_bic(pMObs,pMPrd,fObs,nFree)
 % Bram Zandbelt, bramzandbelt@gmail.com 
 % $Created : Thu 10 Apr 2014 14:29:49 CDT by bram 
 % $Modified: Thu 10 Apr 2014 14:29:49 CDT by bram 
-bic = -2 * sum((pMObs.*fObs) .* log(pMPrd)) + nFree * log(sum(fObs));
+bic = -2 * sum((pMObs*nObs) .* log(pMPrd)) + nFree * log(sum(nObs));
 
