f20396ca43c008fdaabaeb91499707d968f49837
Bram Zandbelt <bramzandbelt@gmail.com>
2014-08-24 12:17:54 -0500

    Bugfix - Did not specify iWlibGo correctly

--- sam_spec_x.m
+++ sam_spec_x.m
@@ -189,8 +189,7 @@ freeCatClass = cellfun(@(in1) in1(:)',freeCatClass,'Uni',0);
 
 % Set lateral inhibition from GO to STOP to 0
 freeCatClass{1,iWlib} = false(size(freeCatClass{1,iWlib}));
-iWlibGO = SAM.model.variants.toFit.XSpec.i.all.iCat{iWlib} ==  ...
-          SAM.model.variants.toFit.XSpec.i.all.iCatClass{1,iWlib};
+iWlibGo = find(~freeCatClass{1,iWlib});
 freeCat{iWlib}(iWlibGO) = false;
 
 % Put variables in output structure
